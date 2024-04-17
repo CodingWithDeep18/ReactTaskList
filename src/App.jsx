624b7d34b92858d8dad5bd938606729d2bc83829
CodingWithDeep18 <deep.shah@bacancy.com>
2024-04-17 15:08:18 +0530

    Created a form using react-hook-form and yup schema

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EducationForm from "./components/EducationForm";
+
+const App = () => {
+  return (
+    <div>
+      <EducationForm />
+    </div>
+  );
+};
+
+export default App;
